ba6da932ef6d6f0111aff173d633324e4492da12
Yauhen Kviatkouski <kvetkovski@gmail.com>
2022-02-27 20:59:03 +0100

    feat: commot styles and components

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,18 @@
 const constants = {
   DEFAULT_BASE_CURRENCY: 'EUR',
   DEFAULT_BASE_CURRENCY_VALUE: 100,
-  MAX_CONVERTING_VALUE_LENGTH: 100,
+  MAX_CONVERTING_VALUE_LENGTH: 11,
   DEFAULT_CURRENCIES_LIST: [
     'EUR',
     'USD',
     'GBP',
     'CHF',
-    'PLN',
     'JPY',
     'CNY',
     'CAD',
     'AUD',
     'SEK',
+    'PLN',
     'RUB',
   ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/index.css';
+import './styles/normalize.css';
+import './styles/stylesVariables.css';
+
+import App from './containers/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
